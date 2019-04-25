7e336c7a9d7a7df42b7f6fac066b791829135b6a
Vadym Liubko <neznam.ua@gmail.com>
2019-04-25 14:59:21 +0300

    [UPD] text

--- 42clean.sh
+++ 42clean.sh
@@ -6,11 +6,10 @@ BEFORE="$(df -h | grep "$(whoami)" | awk {'print $4'})"
 rm -rf $HOME/Library/*42_cache*;
 rm -rf $HOME/.*42_cache*;
 rm -rf $HOME/.*zcompdump*;
-/bin/echo -n $'\e[0mFree space for '
+/bin/echo -n $'\e[0mCleaning space for '
 /bin/echo $'\e[40;38;5;82m' "$(whoami)" $'\e[0m'
-/bin/echo $'(from 5GB)\n'
-/bin/echo $'\e[38;5;196mBefore:' $BEFORE
+/bin/echo $'\e[38;5;196mFree space Before:' $BEFORE
 AFTER="$(df -h | grep "$(whoami)" | awk {'print $4'})"
-/bin/echo $'\e[38;5;46mAfter:' $AFTER
+/bin/echo $'\e[38;5;46mFree space After:' $AFTER
 
 /bin/echo $'\n\e[38;5;226m== Garbage removed! ==\e[0m'
